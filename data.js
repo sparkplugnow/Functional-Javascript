c41f65a237ea3c489be4baf2892bf45112a4c0f4
Israel Adura <israeladura@gmail.com>
2018-02-07 09:06:27 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,64 @@
+const payload = [
+  {
+    time: '2018-02-04',
+    user: 'Jumanji',
+    type: 'meal',
+    notes: 'test notes 1',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-05',
+    user: 'Jumanji',
+    type: 'gallery',
+    notes: 'test notes 1',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-05',
+    user: 'Jumanji',
+    type: 'snack',
+    notes: 'toast, berries, apple sauce',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-04',
+    user: 'Jumanji',
+    type: 'activity',
+    notes: 'test notes 2',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-04',
+    user: 'izzy',
+    type: 'diaper',
+    notes: 'test notes 3',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-04',
+    user: 'Jumanji',
+    type: 'diaper',
+    notes: 'test notes 4',
+    media: 'media.jpg'
+  }, {
+
+    time: '2018-02-06',
+    user: 'Jumanji',
+    type: 'meal',
+    notes: 'cinnamon raison toast, banana, milk',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-06',
+    user: 'Jumanji',
+    type: 'meal',
+    notes: 'test notes 4',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-06',
+    user: 'Jumanji',
+    type: 'meal',
+    notes: 'test notes 4',
+    media: 'media.jpg'
+  }, {
+    time: '2018-02-07',
+    user: 'Jumanji',
+    type: 'diaper',
+    notes: 'test notes 4',
+    media: 'media.jpg'
+  }
+];
---
+++ sample-snippet.js
@@ -0,0 +1,20 @@
+{
+    '2012-05-22': [
+        {
+            text: 'item 1 - any js object'
+        }
+    ],
+    '2012-05-23': [
+        {
+            text: 'item 2 - any js object'
+        }
+    ],
+    '2012-05-24': [],
+    '2012-05-25': [
+        {
+            text: 'item 3 - any js object'
+        }, {
+            text: 'any js object'
+        }
+    ]
+}
